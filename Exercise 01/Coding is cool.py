f2ff95800aa3588a1f908cf941689588e071ce2c
amaan704 <amaansadathofficial@gmail.com>
2024-11-11 11:43:27 +0400

    Coding is cool.py

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,24 @@
+"""Fill in the blanks in the Python code below to output the phrase “Coding is
+Cool” to the console using variables and string concatenation.
+
+1
+
+Fill in the blanks below
+word1 =
+word2 =
+word3 =
+# Use string concatenation to combine the variables and print the phrase
+_____(_________)"""
+
+#Combining the variables and printing the phrase using string concatenation
+
+#Defining 1st word of the phrase
+phrase1 = "Coding"
+
+#Defining 2nd word of the phrase
+phrase2 = "is"
+
+#Defining 3rd word of the phrase
+phrase3 = "cool"
+
+print(phrase1 + " " + phrase2 + " " + phrase3)
