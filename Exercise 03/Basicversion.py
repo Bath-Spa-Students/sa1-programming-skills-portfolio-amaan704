8ae0cfb84a464f2aefca5201ca52b8119b53502e
amaan704 <amaansadathofficial@gmail.com>
2024-11-19 15:34:12 +0400

    Basicversion.py

---
+++ Exercise 03/Basicversion.py
@@ -0,0 +1,11 @@
+# Step 1: Allocate the information in a dictionary
+person_info = {
+    "name": "AMAAN",
+    "hometown": "KERALA",
+    "age": 19
+}
+
+# Step 2: display the values on separate lines using a single print statement
+print(f"Name: {person_info['name']}")
+print(f"Hometown: {person_info['hometown']}")
+print(f"Age: {person_info['age']}")
