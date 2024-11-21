f770285cb6d2972be47dae9162cfa02d3635dd0a
amaan704 <amaansadathofficial@gmail.com>
2024-11-21 12:18:32 +0400

    Some Counting.py

---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,29 @@
+# Loop  counts up from 0 to 50 in addition of 1
+print("Counting up from 0 to 50 in addition  by 1:")
+for i in range(0, 51, 1):
+    print(i,end=" ")
+print("\n")
+
+# Loop  counts down from 50 to 0 by reduction of 1
+print("Counting down from 50 to 0 by reduction 1:")
+for i in range(50, -1, -1):
+    print(i,end=" ")
+print("\n")
+
+# Loop  counts up from 30 to 50 addition of 1
+print("Counting up from 30 to 50 addition of 1:")
+for i in range(30, 51, 1):
+    print(i,end=" ")
+print("\n")
+
+# Loop  counts down from 50 to 10 by decreases  of 2
+print("Counting down from 50 to 10 by decreases  of 2:")
+for i in range(50, 9, -2):
+    print(i,end=" ")
+print("\n")
+
+# Loop  counts up from 100 to 200 in increments of 5
+print("Counting up from 100 to 200 in addition of 5:")
+for i in range(100, 201, 5):
+    print(i, end=" ")
+print("\n")
