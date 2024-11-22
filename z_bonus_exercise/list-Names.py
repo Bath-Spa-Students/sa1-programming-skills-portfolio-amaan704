d63c6b2d3846d99bb8c936b154ee53198a957fda
amaan704 <amaansadathofficial@gmail.com>
2024-11-22 16:04:40 +0400

    list-Names.py

---
+++ z_bonus_exercise/list-Names.py
@@ -0,0 +1,8 @@
+# give  names of friends in a list
+names = ["Shibla", "Sinan", "Anashwara", "Rifa"]
+
+# publish each person's name by accessing each element
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
