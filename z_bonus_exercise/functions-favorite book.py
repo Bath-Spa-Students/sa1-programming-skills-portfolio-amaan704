876d25297a3340252ea49a31d152468f971da78e
amaan704 <amaansadathofficial@gmail.com>
2024-11-22 16:03:55 +0400

    functions-favorite book.py

---
+++ z_bonus_exercise/functions-favorite book.py
@@ -0,0 +1,6 @@
+# Specify the function favorite_book
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+# Mention the function with a book title
+favorite_book("Alice in Wonderland")
