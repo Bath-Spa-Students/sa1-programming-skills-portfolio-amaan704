f3e8dbed771c9a832ba6fedcae0aa178f22f4687
amaan704 <amaansadathofficial@gmail.com>
2024-11-22 16:06:04 +0400

    Variables.py

---
+++ z_bonus_exercise/Variables.py
@@ -0,0 +1,7 @@
+# give a message to a variable
+message = "I am Amaan Sadath."
+print(message)
+
+# change the value of the variable to a new message
+message = "I am nineteen years old."
+print(message)
