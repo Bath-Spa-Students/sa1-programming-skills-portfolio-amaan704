c543601b9d5c2b885f5a505853f8108401cfdbcd
amaan704 <amaansadathofficial@gmail.com>
2024-11-22 16:02:14 +0400

    Dictionaries.py

---
+++ z_bonus_exercise/Dictionaries.py
@@ -0,0 +1,13 @@
+# collect figures about a person in dictionary
+person = {
+    "first_name": "Amaan ",
+    "last_name": "Sadath",
+    "age": 19,
+    "city": "ras al khaimah"
+}
+
+# Print all figures given in the dictionary
+print("First Name:", person["first_name"])
+print("Last Name:", person["last_name"])
+print("Age:", person["age"])
+print("City:",person["city"])
